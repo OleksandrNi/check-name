21738946f915bcc7ce7c12ac9cdd4f846f41112b
OleksandrNi <nika200382@gmail.com>
2023-09-05 17:43:09 +0300

    add insta

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -1,13 +1,17 @@
 import { NextResponse } from "next/server";
 
-const axios = require('axios');
+const axios = require("axios");
 
-const bearerToken = "AAAAAAAAAAAAAAAAAAAAADAhpwEAAAAAaLmoLOZcw4tulISk3orJaBaI5qA%3Dz0iaDGrsSh0QgqG8xEqk1LRovpQe94Yx5l58JBS3ShvN78LfMi";
+const bearerToken =
+  "AAAAAAAAAAAAAAAAAAAAADAhpwEAAAAAaLmoLOZcw4tulISk3orJaBaI5qA%3Dz0iaDGrsSh0QgqG8xEqk1LRovpQe94Yx5l58JBS3ShvN78LfMi";
 
 export async function POST(req, res) {
   const body = await req.json();
-  const screenName = body.twitterUsername;
-  console.log('screenName', screenName)
+  const screenName = body.username;
+  console.log("screenName", screenName);
+
+  let checkResponse = [];
+
   try {
     const response = await axios.get(
       `https://api.twitter.com/1.1/users/show.json?screen_name=${screenName}`,
@@ -20,12 +24,32 @@ export async function POST(req, res) {
 
     if (response.status === 200) {
       console.log(response.data);
-      return NextResponse.json({ message: `this account ${response.data.name} with id${response.data.id}` });
+      checkResponse.push(
+        `in Twitter this account ${response.data.name} with id${response.data.id}`
+      );
     } else {
       console.log(`Error: Unexpected status code ${response}`);
     }
   } catch (error) {
     console.error(`User not exist`);
-    return NextResponse.json({ message: `User not exist` });
+    checkResponse.push(`User not exist in Twitter`);
   }
+
+  const response = await axios.get(
+    `https://www.instagram.com/${screenName}/?__a=1`
+  );
+  const responseText = await response.data;
+  const jsonStartIndex = responseText.indexOf("{");
+  const cleanedResponseText = responseText.substring(jsonStartIndex);
+
+  try {
+    const jsonData = JSON.parse(cleanedResponseText);
+    checkResponse.push(`User not exist in Insta`);
+    console.log(jsonData.error);
+  } catch (error) {
+    checkResponse.push(`User exist in Insta`);
+    console.error("Ошибка при парсинге JSON:", error.message);
+  }
+
+  return NextResponse.json(checkResponse);
 }
--- app/page.js
+++ app/page.js
@@ -5,28 +5,32 @@ import styles from "./page.module.css";
 import axios from "axios";
 
 export default function Home() {
-  const [twitterUsername, setTwitterUsername] = useState("");
+  const [username, setUsername] = useState("");
   const [twitterInfo, setTwitterInfo] = useState(null);
+  const [instaInfo, setInstaInfo] = useState(null);
 
-  const handleTwitterCheck = async () => {
+  const handleNameCheck = async () => {
     // const { data } = await axios.post(`http://localhost:3000/api/search`, {
     const { data } = await axios.post(`https://check-name.vercel.app//api/search`, {
-      twitterUsername,
+      username,
     });
-    setTwitterInfo(data.message);
+    console.log(data)
+    setTwitterInfo(data[0]);
+    setInstaInfo(data[1]);
   };
 
   return (
     <main className={styles.main}>
       <input
         type="text"
-        placeholder="enter name Twitter"
-        value={twitterUsername}
-        onChange={(e) => setTwitterUsername(e.target.value)}
+        placeholder="enter name for check"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
-      <button onClick={handleTwitterCheck}>Check name in Twitter</button>
+      <button onClick={handleNameCheck}>Check name in SM</button>
       Info
-      {twitterInfo && <div>{twitterInfo}</div>}
+      {twitterInfo &&  <div>{twitterInfo}</div>}
+      {instaInfo &&  <div>{instaInfo}</div>}
     </main>
   );
 }
